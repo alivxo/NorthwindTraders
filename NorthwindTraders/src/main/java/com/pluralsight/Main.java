cec5945ddce9e5d792370c98530e1556a9ac8ba8
Alivxo <avsal0218@gmail.com>
2024-06-11 10:37:54 -0400

    added switch case

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -4,14 +4,103 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    final static Scanner scanner = new Scanner (System.in);
     public static void main(String[] args) {
+        homeScreen();
+        }
 
+        private static void homeScreen() {
+            String homeScreen = """
+                Welcome to Northwind Traders Database
+                -------------------------------------
+                What do you want to do?
+                1. Display all products
+                2. Display all customers
+                0. Exit 
+                Select an option:
+                """;
+            System.out.println(homeScreen);
+            String userchoice = scanner.nextLine();
+
+            boolean choice = true;
+
+            while(choice) {
+                switch (userchoice) {
+                    case "1":
+                        displayproducts();
+                        break;
+                    case "2":
+                        displaycustomers();
+                        break;
+                    case "0":
+                        choice = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option, please try again");
+                }
+            }
+        }
+
+    private static void displaycustomers() {
         String url = "jdbc:mysql://localhost:3306/northwind";
+        String username = "yourUsername"; // Replace with your MySQL username
+        String password = "yourPassword"; // Replace with your MySQL password
+
+        try {
+            // Load the MySQL Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 1. Open a connection to the database
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // 2. Create a statement
+            Statement statement = connection.createStatement();
+
+            // 3. Define your query
+            String query = "SELECT CustomerID, CompanyName, ContactName, Country FROM customers";
+
+            // 4. Execute your query
+            ResultSet results = statement.executeQuery(query);
+
+            // Option 1: Display stacked information
+            System.out.println("Option 1: Stacked Information\n");
+            while (results.next()) {
+                String customerID = results.getString("CustomerID");
+                String companyName = results.getString("CompanyName");
+                String contactName = results.getString("ContactName");
+                String phoneNumber = results.getString("PhoneNumber");
+                String city = results.getString("City");
+                String country = results.getString("Country");
+
+                System.out.println("Customer ID: " + customerID);
+                System.out.println("Company Name: " + companyName);
+                System.out.println("Contact Name: " + contactName);
+                System.out.println("Phone Number: " + phoneNumber);
+                System.out.println("Country: " + country);
+                System.out.println("City: " + city);
+                System.out.println("------------------");
+            }
+            // 5. Close the connection
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Connection failed.");
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void displayproducts() {
+
+    String url = "jdbc:mysql://localhost:3306/northwind";
         String username = "root";
         String password = "YUm15510n";
 
@@ -72,4 +161,6 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+
 }
